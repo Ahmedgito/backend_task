a1b618c6dbe61294300c1014e3a427dea26e4545
Ahmedgito <mamooabid1@gmail.com>
2025-05-20 16:00:05 +0500

    Fix product creation API endpoint and improve formatting in server.js

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 const express = require('express')
 const fs = require("fs");
 const path = require("path");
-const app = express() ;
+const app = express();
 
-app.use(express.json) ;
-app.use(express.urlencoded({extended : true })) ;
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
 
 
 const DATA_FILE = path.join(__dirname, "products.json"); //from here ill import data from json file in which i have my products
- 
+
 let products = [];
 let useId = 1;
 
 // RestApi 
 
 // Creat API for creating a product
-app.post("/create")
+app.post("/products", (req, res) => {
+    const products = readProducts();
+
+    const newProduct = {
+
+        id: Date.now(), 
+        name,
+        price,
+    };
+
+    products.push(newProduct);
+    writeProducts(products);
+
+    res.status(201).json(newProduct);
+})
+
+//Fetching PRoducts
+app.get("/products", (req, res) => {
+
+  const products = readProducts();
+  res.json(products);
+  
+});
