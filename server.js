02dd452e03b2df0866d3e4ecc3ad0eeba43b0e1a
Ahmedgito <mamooabid1@gmail.com>
2025-05-20 15:58:34 +0500

    Refactor server.js: add missing fs and path imports, rename FILE_PATH to DATA_FILE, and update comment for product creation API

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express')
+const fs = require("fs");
+const path = require("path");
 const app = express() ;
 
 app.use(express.json) ;
 app.use(express.urlencoded({extended : true })) ;
 
 
-const FILE_PATH = path.join(__dirname, "products.json"); //from here ill import data from json file in which i have my products
+const DATA_FILE = path.join(__dirname, "products.json"); //from here ill import data from json file in which i have my products
  
 let products = [];
 let useId = 1;
 
 // RestApi 
 
-// Creat API
-app.post
+// Creat API for creating a product
+app.post("/create")
